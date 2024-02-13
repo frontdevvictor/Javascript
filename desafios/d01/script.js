ddf32a9c025b35e27b9aa0055ec9753610384089
Victor Hugo <frontdevvictor@gmail.com>
2024-02-12 22:30:06 -0300

    última atualização

--- desafios/d01/script.js
+++ desafios/d01/script.js
@@ -1,17 +1,20 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+    var img = window.document.getElementById('imagemDia')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12){
         // BOM DIA!
-        img.src = 'Manhã.png'
+        img.src = 'imagens/Manhã.png'
+        document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora < 18){
         // BOA TARDE!
-        img.src = 'Tarde.png'
+        img.src = 'imagens/Tarde.png'
+        document.body.style.background = '#b9846f'
     } else {
         // BOA NOITE!
-        img.src = 'Noite.png'
+        img.src = 'imagens/Noite.png'
+        document.body.style.background = '#515154'
     }
 }
