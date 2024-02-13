99bbb2468c4630886904f9ba139a9f3133b748de
Victor Hugo <frontdevvictor@gmail.com>
2024-02-12 21:42:44 -0300

    Exercícios JavaScript (Parte 2) - Atualizações

--- desafios/d01/script.js
+++ desafios/d01/script.js
@@ -1,1 +1,7 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('img')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+}
