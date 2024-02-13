89a1660021611c48969a650501b3daf7e8089c8d
Victor Hugo <frontdevvictor@gmail.com>
2024-02-13 19:20:26 -0300

    Atualização no JS

--- desafios/d02/d01/script.js
+++ desafios/d02/d01/script.js
@@ -1,22 +1,29 @@
-function verificar(){
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || fano.value > ano){
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
+function verificar() {
+    var data = new Date();
+    var ano = data.getFullYear();
+    var fano = document.getElementById('txtano');
+    var res = document.querySelector('div#res');
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!');
     } else {
-        window.alert('Tudo ok!')
+        var fsex = document.getElementsByName('radsex');
+        var idade = ano - Number(fano.value);
+        var gênero = 'Indefinido'; // Inicializa com valor padrão
+        if (fsex[0].checked) {
+            gênero = 'Homem';
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher';
+        }
+        res.innerHTML = `Detectando ${gênero} com ${idade} anos.`; // Utiliza template literals
     }
-
 }
 
 function Limpar() {
     var fano = document.getElementById('txtano');
-    var fmas = document.getElementById('mas');
-    var ffem = document.getElementById('fem');
+    var masculinoCheckbox = document.getElementById('mas');
+    var femininoCheckbox = document.getElementById('fem');
     
     fano.value = ""; // Limpa o campo de ano de nascimento
-    fmas.checked = false; // Desmarca o sexo masculino
-    ffem.checked = false; // Desmarca o sexo feminino
+    masculinoCheckbox.checked = false; // Desmarca o sexo masculino
+    femininoCheckbox.checked = false; // Desmarca o sexo feminino
 }
