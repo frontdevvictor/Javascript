cf515025ac5fdd6bf08edaaaaf06cc09a579d502
Victor Hugo <frontdevvictor@gmail.com>
2024-02-18 15:29:32 -0300

    atualização do código JS (tabuada)

---
+++ desafios/d04/d03/script.js
@@ -0,0 +1,10 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.lenght == 0){
+        let n = Number(num.value)
+
+    } else {
+        window.alert('Por favor, digite um número!')
+    }
+}
