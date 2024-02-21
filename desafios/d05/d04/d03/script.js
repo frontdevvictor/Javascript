71282047f2f23fb0632c3b7f4feb65509fbe3260
Victor Hugo <frontdevvictor@gmail.com>
2024-02-21 10:56:11 -0300

    Atualizada

--- desafios/d05/d04/d03/script.js
+++ desafios/d05/d04/d03/script.js
@@ -4,7 +4,7 @@ let res = document.querySelector('div#res');
 let valores = [];
 
 function isNumero(n) {
-    if(Number(n) >= 1 && Number(n) <= 100) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
         return true;
     } else {
         return false;
@@ -20,12 +20,23 @@ function inLista(n, lista) {
 }
 
 function adicionar() {
-    if(isNumero(num.value) && !inLista(num.value, valores)) {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
         valores.push(Number(num.value));
         let item = document.createElement('option');
-        item.text = `Valor ${num.value} adicionado.`; // Corrigindo interpolação de string
-        list.appendChild(item); // Corrigindo seletor do elemento select
+        item.text = `Valor ${num.value} adicionado.`;
+        list.appendChild(item);
+        num.value = ''; // Limpar o campo de entrada após adicionar o valor
+        num.focus(); // Colocar o foco de volta no campo de entrada
     } else {
         window.alert('Valor inválido ou já encontrado na lista.');
+    }
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar.');
+    } else {
+        let tot = valores.length;
+        res.innerHTML = `<p>Ao todo temos ${tot} números cadastrados.</p>`;
     }
 }
